71cf9602f78ee6efd6885625e2e67dbedcb66b0a
Ndeye Penda SARR <ndasarr884@gmail.com>
2026-08-02 00:31:23 +0000

    Ignore le dossier .next (cache de build)

--- .next/dev/static/chunks/app_layout_tsx_007e4b2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0j7a64b._.css","static/chunks/node_modules_next_dist_1nyev3z._.js","static/chunks/components_ThemeProvider_tsx_197_d_9._.js"],
-    source: "dynamic"
-});
